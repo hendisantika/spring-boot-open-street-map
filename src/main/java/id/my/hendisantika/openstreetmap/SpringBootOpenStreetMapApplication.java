dede5305398dd4455acb8e47dc8579d7eef50e8b
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 15:16:18 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/openstreetmap/SpringBootOpenStreetMapApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.openstreetmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenStreetMapApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenStreetMapApplication.class, args);
+    }
+
+}

1fe0a5fb96b5624e1a06344cb0060d090f43892b
hendisantika <hendisantika@yahoo.co.id>
2025-10-07 15:20:31 +0700

    feat: Add MainController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/openstreetmap/controller/MainController.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.openstreetmap.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-open-street-map
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 07/10/25
+ * Time: 15.20
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class MainController {
+}
